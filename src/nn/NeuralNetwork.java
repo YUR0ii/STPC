02725feb3011ba03c823bcd379dde9bd81b6a27e
David Aylaian <davidaylaian@protonmail.com>
2019-04-04 13:10:16 -0400

    Neural Net work

--- src/nn/NeuralNetwork.java
+++ src/nn/NeuralNetwork.java
@@ -7,43 +7,51 @@ import org.jblas.MatrixFunctions;
 public class NeuralNetwork
 {
 	// TODO: more options for activation functions
-	
+
 	private Random prng;
-	
+
 	/*
 	 * TODO: Later on, having more layers or support for an
 	 * arbitrary number of layers could be worth the effort
 	 */
-	
+
 	public int input_neurons;
 	public int hidden_neurons;
 	public int output_neurons;
-	
+
 	// weights and biases for each layer
 	private DoubleMatrix weights1;
 	private DoubleMatrix weights2;
 	private DoubleMatrix biases1;
 	private DoubleMatrix biases2;
-	
+
 	public NeuralNetwork()
 	{
 		// we seed the prng for consistency
 		prng = new Random(0);
 	}
-	
+
 	private DoubleMatrix input_layer;
 	private DoubleMatrix hidden_layer;
 	private DoubleMatrix output_layer;
-	
+
 	public void forward_prop(DoubleMatrix input)
 	{
 		input_layer = input;
 		hidden_layer = MatrixFunctions.tanh(input_layer.mmul(weights1).add(biases1));
 		output_layer = MatrixFunctions.tanh(hidden_layer.mmul(weights2).add(biases2));
 	}
-	
+
+	public static DoubleMatrix tanh_derivative(DoubleMatrix A)
+	{
+		// TODO: optimizations and caching
+		DoubleMatrix tanh = MatrixFunctions.tanh(A);
+		DoubleMatrix ones = DoubleMatrix.ones(A.rows, A.columns);
+		return ones.sub(tanh.mul(tanh));
+	}
+
 	public void backward_prop()
 	{
-		
+		DoubleMatrix delta_output = tanh_derivative(output_layer);
 	}
 }
--- src/nn/XOR.java
+++ src/nn/XOR.java
@@ -1,12 +1,14 @@
 package nn;
 
+import org.jblas.DoubleMatrix;
+
 public class XOR
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		NeuralNetwork nn = new NeuralNetwork();
 		nn.input_neurons = 2;
 		nn.hidden_neurons = 3;
-		nn.output_neurons = 2;
+		nn.output_neurons = 1;
 	}
 }
