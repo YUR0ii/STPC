741c379e72ad88b866c02f174864c56be934ac46
David Aylaian <davidaylaian@protonmail.com>
2019-04-12 13:08:30 -0400

    Backpropogation debugging

--- src/nn/NeuralNetwork.java
+++ src/nn/NeuralNetwork.java
@@ -24,7 +24,7 @@ public class NeuralNetwork
 	// the layers
 	protected DoubleMatrix A0;	// input layer
 	protected DoubleMatrix A1;	// hidden layer
-	protected DoubleMatrix A2;	// output layer
+	public DoubleMatrix A2;		// output layer
 
 	// loss
 	protected DoubleMatrix loss;
@@ -42,11 +42,13 @@ public class NeuralNetwork
 	public void feedforward(DoubleMatrix input)
 	{
 		A0 = input;
-		A1 = MatrixFunctions.tanh(A0.mmul(W0.transpose()).addRowVector(B0));
-		A2 = MatrixFunctions.tanh(A1.mmul(W1.transpose()).addRowVector(B1));
+		//A1 = MatrixFunctions.tanh(A0.mmul(W0.transpose()).addRowVector(B0));
+		//A2 = MatrixFunctions.tanh(A1.mmul(W1.transpose()).addRowVector(B1));
+		A1 = MatrixFunctions.tanh(A0.mmul(W0.transpose()));
+		A2 = MatrixFunctions.tanh(A1.mmul(W1.transpose()));
 	}
 
-	public void backprop(DoubleMatrix desired_output)
+	public void backprop(DoubleMatrix desired_output, TrainingHyperparameters thp)
 	{
 		// implementation of the squared loss function
 		DoubleMatrix loss = DoubleMatrix.zeros(A2.rows);
@@ -60,14 +62,19 @@ public class NeuralNetwork
 		DoubleMatrix d_loss = A2.sub(desired_output).mul(2);
 		DoubleMatrix d_loss_d_A2 = d_loss.mul(d_tanh(A2));
 		DoubleMatrix d_W1 = A1.transpose().mmul(d_loss_d_A2);
-
-		System.out.println(d_loss_d_A2.rows);
-		System.out.println(d_loss_d_A2.columns);
-		System.out.println(W1.transpose().rows);
-		System.out.println(W1.transpose().columns);
-
 		DoubleMatrix tmp = d_loss_d_A2.mmul(W1);
-		DoubleMatrix d_W0 = A0.transpose().mmul(tmp).mmul(d_tanh(A1));
+		DoubleMatrix tmp2 = (tmp).mul(d_tanh(A1));
+		DoubleMatrix d_W0 = A0.transpose().mmul(tmp2);
+
+//		System.out.println(d_W0.rows);
+//		System.out.println(d_W0.columns);
+//		System.out.println(W0.rows);
+//		System.out.println(W0.columns);
+
+		//System.out.println(d_loss);
+
+		W1.add(d_W1.transpose().mul(thp.learning_rate));
+		W0.add(d_W0.transpose().mul(thp.learning_rate));
 	}
 
 	// hyperparameters for training
@@ -87,16 +94,22 @@ public class NeuralNetwork
 		org.jblas.util.Random.seed(0);
 
 		// generate random weights and biases
-		W0 = DoubleMatrix.rand(hidden_neurons, input_neurons);
-		B0 = DoubleMatrix.rand(hidden_neurons);
-		W1 = DoubleMatrix.rand(output_neurons, hidden_neurons);
-		B1 = DoubleMatrix.rand(output_neurons);
+//		W0 = DoubleMatrix.rand(hidden_neurons, input_neurons);
+//		B0 = DoubleMatrix.rand(hidden_neurons);
+//		W1 = DoubleMatrix.rand(output_neurons, hidden_neurons);
+//		B1 = DoubleMatrix.rand(output_neurons);
+
+		//W0 = W0.mul(2).sub(1);
+		//W1 = W1.mul(2).sub(1);
+
+		W0 = DoubleMatrix.randn(hidden_neurons, input_neurons);
+		W1 = DoubleMatrix.randn(output_neurons, hidden_neurons);
 
 		// main training loop
 		for (int i = 0; i < thp.epochs; i++)
 		{
 			feedforward(input_data);
-			backprop(output_data);
+			backprop(output_data, thp);
 		}
 	}
 }
--- src/nn/XOR.java
+++ src/nn/XOR.java
@@ -10,7 +10,7 @@ public class XOR
 		nn.input_neurons = 2;
 		nn.hidden_neurons = 3;
 		nn.output_neurons = 1;
-		
+
 		DoubleMatrix inputs = new DoubleMatrix(
 			new double[][] {
 				{0, 0},
@@ -19,7 +19,7 @@ public class XOR
 				{1, 1}
 			}
 		);
-		
+
 		DoubleMatrix outputs = new DoubleMatrix(
 			new double[][] {
 				{0},
@@ -28,13 +28,22 @@ public class XOR
 				{0}
 			}
 		);
-		
+
 		NeuralNetwork.TrainingHyperparameters thp = nn.new TrainingHyperparameters();
-		thp.epochs = 40_000;
-		thp.learning_rate = 0.1;
-		
+		thp.epochs = 100_000;
+		thp.learning_rate = 0.01;
+
 		long nt = System.nanoTime();
 		nn.train(inputs, outputs, thp);
 		System.out.println("Finished training in " + (System.nanoTime() - nt) / 1_000_000 + " ms");
+
+		nn.feedforward(inputs);
+
+		for (int i = 0; i < outputs.length; i++)
+		{
+			System.out.println("\nTest");
+			System.out.println(nn.A2.get(i));
+			System.out.println(outputs.get(i));
+		}
 	}
 }
